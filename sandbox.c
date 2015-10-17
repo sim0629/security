be9763b84632cf535d308da06b197a44169ceaf8
Gyumin Sim <sim0629@gmail.com>
2015-10-18 00:56:41 +0900

    Block or detect dangerous syscalls

--- sandbox.c
+++ sandbox.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ptrace.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
@@ -11,6 +12,10 @@
 #include "sgm_syscallent.h"
 
 static pid_t tracee;
+
+static struct pcy {
+    bool kill;
+} my_pcy;
 
 void suicide() {
     if(kill(tracee, SIGKILL) < 0) {
@@ -63,20 +68,59 @@ void print_syscall_entry(struct user_regs_struct *regs_p) {
         for(i = 0; i < 6; i++) {
             printf("0x%Lx", args[i]);
             if(i < 5) printf(", ");
-            else printf(" )");
         }
-        printf("?");
+        printf(" )?");
     }else {
         for(i = 0; i < ents[n].argc; i++) {
             printf("0x%Lx", args[i]);
             if(i < ents[n].argc - 1) printf(", ");
-            else printf(" )");
         }
+        printf(" )");
     }
 }
 
 void print_syscall_exit(struct user_regs_struct *regs_p) {
     printf(" = 0x%Lx\n", regs_p->rax);
+}
+
+void print_encounter(const char *type) {
+    printf(" => Encountered a %s syscall", type);
+}
+
+void sandbox(struct user_regs_struct *regs_p) {
+    unsigned long long n = regs_p->orig_rax;
+
+    switch(n) {
+    case SYS_open:
+    case SYS_creat:
+    case SYS_openat:
+        print_encounter("filesystem");
+        if(my_pcy.kill) suicide();
+        break;
+
+    case SYS_socket:
+    case SYS_socketpair:
+        print_encounter("network");
+        if(my_pcy.kill) suicide();
+        break;
+
+    case SYS_pipe:
+    case SYS_pipe2:
+    case SYS_shmget:
+    case SYS_msgget:
+    case SYS_mq_open:
+        print_encounter("IPC");
+        if(my_pcy.kill) suicide();
+        break;
+
+    case SYS_mmap:
+        print_encounter("memory mapping");
+        if(my_pcy.kill) suicide();
+        break;
+
+    default:
+        break;
+    }
 }
 
 void monitor() {
@@ -101,6 +145,7 @@ void monitor() {
         if(entry) {
             get_regs(&regs_entry);
             print_syscall_entry(&regs_entry);
+            sandbox(&regs_entry);
         }else {
             get_regs(&regs_exit);
             print_syscall_exit(&regs_exit);
@@ -108,7 +153,26 @@ void monitor() {
     }
 }
 
+void usage(const char *exe) {
+    fprintf(stderr, "usage: %s [-k] <tracee>\n", exe);
+}
+
 int main(int argc, char *argv[]) {
+    const char *exe = argv[0];
+    int offset = 1;
+
+    if(argc < 2) {
+        usage(exe);
+        return 1;
+    }
+    if(strcmp(argv[1], "-k") == 0) {
+        my_pcy.kill = true;
+        offset = 2;
+        if(argc < 3) {
+            usage(exe);
+            return 1;
+        }
+    }
 
     if((tracee = fork()) < 0) {
         perror("fork");
@@ -120,7 +184,7 @@ int main(int argc, char *argv[]) {
             perror("ptrace traceme");
             return 1;
         }
-        if(execvp(argv[1], argv + 1) < 0) {
+        if(execvp(argv[offset], argv + offset) < 0) {
             perror("execvp");
             return 0;
         }
