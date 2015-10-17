3cc9006088f2351242dfc4f54c1e4e02e46e0ec3
Gyumin Sim <sim0629@gmail.com>
2015-10-18 02:13:39 +0900

    Bypass private mmap

--- sandbox.c
+++ sandbox.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/ptrace.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
@@ -114,6 +115,7 @@ void sandbox(struct user_regs_struct *regs_p) {
         break;
 
     case SYS_mmap:
+        if(regs_p->r10 & MAP_PRIVATE) break;
         print_encounter("memory mapping");
         if(my_pcy.kill) suicide();
         break;
